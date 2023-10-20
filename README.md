b090a7c58fa571399a8eaae9ad120e309741b950
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-10-19 21:44:50 -0400

    pizza, pasta

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 #  muniverse
 ## muniverse is cool
 ### muniverse is superb ammzing omg like and subscribe.
-#### virat kohli supremacy!
--- web/pages/pricing.js
+++ web/pages/pricing.js
@@ -9,14 +9,27 @@ export default function Pricing() {
             <Navbar />
 
             <div className={`overflow-hidden ${font_500}`}>
-                <h1 className={`${font_500} font-bold text-5xl mt-24 text-center`}>Pricing</h1>
+                <h1 className={`${font_500} font-bold text-7xl mt-24 text-center`}>Pricing</h1>
                 <p className={`text-center mt-2`}>We at muniverse price each service individually, so you can mix and match what you want.</p>
-                <div className='grid grid-cols-3'>
-                    <div><p>hi!</p></div>
-                    <div><p>hi!</p></div>
-                    <div><p>hi!</p></div>
+                <div className='grid grid-cols-3 mt-16'>
+                    <div className='flex justify-center'>
+                        <div className='w-9/12 rounded-lg pt-5 pb-5 h-96  border-2 border-red-500 shadow-2xl shadow-red-500'>
+                            <h1 className='text-center text-4xl'>software</h1>
+                        </div>
+                    </div>
+                    <div className='flex justify-center'>
+                        <div className='w-9/12 rounded-lg pt-5 pb-5 h-96  border-2 border-blue-500 shadow-2xl shadow-blue-500'>
+                            <p>hi!</p>
+                        </div>
+                    </div>
+                    <div className='flex justify-center'>
+                        <div className='w-9/12 rounded-lg pt-5 pb-5 h-96  border-2 border-green-500 shadow-2xl shadow-green-500'>
+                            <p>hi!</p>
+                        </div>
+                    </div>
                 </div>
 
+                <button>hi!</button>
             </div>
         </div>
     )
