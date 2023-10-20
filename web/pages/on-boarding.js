4e8180cf288a639e3d9fabb07121a6c5255b9233
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-10-20 13:07:49 -0400

    some more stuff - sama called me a 'cucumber'! ugh why don't I have amazing social skills

--- web/pages/on-boarding.js
+++ web/pages/on-boarding.js
@@ -8,10 +8,30 @@ export default function Onboarding() {
             <Navbar />
             <div className={`overflow-hidden ${font_500}`}>
                 <div className='mt-24'>
-                    <p className='text-5xl text-center'>Welcome to muniverse!</p>
+                    <p className='text-6xl text-center'>Welcome to muniverse!</p>
                     <p className={`text-lg text-center ${font_300}`}>Before we get started, we need some information. So please fill out this (boring) form.</p>
 
-                    <input type="text"></input>
+                    <form>
+                        <div className="mt-10 ml-10">
+                            <h1 className='mb-5 text-3xl'>School Information</h1>
+                            <div className="mb-4 ">
+                                <label
+                                    className="block text-gray-700 text-md mb-2 "
+                                    htmlFor="name"
+                                >
+                                    name of your school
+                                </label>
+                                <input
+                                    className={`px-3 py-2 border border-gray-300 rounded-md outline-none focus:outline-none focus:border-red-500 transition-all text-sm w-96 ${font_300}`}
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                    placeholder="Enter the name of your school"
+                                    required
+                                />
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
