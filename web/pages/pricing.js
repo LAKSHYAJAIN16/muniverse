8ad39b78ed5543eebe458050c53a7828e61dbfcd
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-10-19 22:33:12 -0400

    mostly just the images and some content

--- web/pages/pricing.js
+++ web/pages/pricing.js
@@ -37,54 +37,55 @@ export default function Pricing() {
                             <p className={`mt-10 text-center text-7xl text-red-500 ${font_500}`}>$5 <span className={`text-sm ${font_400}`}>/conference</span></p>
                         </div>
                     </div>
+
                     <div className='flex justify-center'>
                         <div className='cursor-pointer w-9/12 rounded-lg pt-5 pb-5 h-96  border-2 border-blue-500 shadow-2xl shadow-blue-500'>
-                            <h1 className='text-center text-5xl'>software</h1>
+                            <h1 className='text-center text-5xl'>virtual MUN</h1>
                             <ul className='pl-5 mt-6'>
                                 <li className='flex items-center mb-3'>
-                                    <img src="/check_3.png" className='h-6'></img>
-                                    <p className={`pl-3 ${font_400}`}>paperless conferences</p>
+                                    <img src="/check_4.png" className='h-6'></img>
+                                    <p className={`pl-3 ${font_400}`}>custom MUN platform</p>
                                 </li>
                                 <li className='flex items-center mb-3'>
-                                    <img src="/check_3.png" className='h-6'></img>
-                                    <p className={`pl-3 ${font_400}`}>digital EB grading software</p>
+                                    <img src="/check_4.png" className='h-6'></img>
+                                    <p className={`pl-3 ${font_400}`}>custom chit and DR system</p>
                                 </li>
                                 <li className='flex items-center mb-3'>
-                                    <img src="/check_3.png" className='h-6'></img>
-                                    <p className={`pl-3 ${font_400}`}>delegate software</p>
+                                    <img src="/check_4.png" className='h-6'></img>
+                                    <p className={`pl-3 ${font_400}`}>integrated design</p>
                                 </li>
                                 <li className='flex items-center mb-3'>
-                                    <img src="/check_3.png" className='h-6'></img>
-                                    <p className={`pl-3 ${font_400}`}>automated fact-checking</p>
+                                    <img src="/check_4.png" className='h-6'></img>
+                                    <p className={`pl-3 ${font_400}`}>specially designed</p>
                                 </li>
                             </ul>
 
-                            <p className={`mt-10 text-center text-7xl text-blue-500 ${font_500}`}>$5 <span className={`text-sm ${font_400}`}>/conference</span></p>
+                            <p className={`mt-10 text-center text-7xl text-blue-500 ${font_500}`}>$20 <span className={`text-sm ${font_400}`}>/conference</span></p>
                         </div>
                     </div>
                     <div className='flex justify-center'>
                         <div className='cursor-pointer w-9/12 rounded-lg pt-5 pb-5 h-96  border-2 border-green-500 shadow-2xl shadow-green-500'>
-                            <h1 className='text-center text-5xl'>software</h1>
+                            <h1 className='text-center text-5xl'>planning </h1>
                             <ul className='pl-5 mt-6'>
                                 <li className='flex items-center mb-3'>
-                                    <img src="/check_3.png" className='h-6'></img>
-                                    <p className={`pl-3 ${font_400}`}>paperless conferences</p>
+                                    <img src="/check_5.png" className='h-6'></img>
+                                    <p className={`pl-3 ${font_400}`}>contact with sponsors</p>
                                 </li>
                                 <li className='flex items-center mb-3'>
-                                    <img src="/check_3.png" className='h-6'></img>
-                                    <p className={`pl-3 ${font_400}`}>digital EB grading software</p>
+                                    <img src="/check_5.png" className='h-6'></img>
+                                    <p className={`pl-3 ${font_400}`}>financial management</p>
                                 </li>
                                 <li className='flex items-center mb-3'>
-                                    <img src="/check_3.png" className='h-6'></img>
-                                    <p className={`pl-3 ${font_400}`}>delegate software</p>
+                                    <img src="/check_5.png" className='h-6'></img>
+                                    <p className={`pl-3 ${font_400}`}>providing suppliers</p>
                                 </li>
                                 <li className='flex items-center mb-3'>
-                                    <img src="/check_3.png" className='h-6'></img>
-                                    <p className={`pl-3 ${font_400}`}>automated fact-checking</p>
+                                    <img src="/check_5.png" className='h-6'></img>
+                                    <p className={`pl-3 ${font_400}`}>conference organization</p>
                                 </li>
                             </ul>
 
-                            <p className={`mt-10 text-center text-7xl text-green-500 ${font_500}`}>$5 <span className={`text-sm ${font_400}`}>/conference</span></p>
+                            <p className={`mt-10 text-center text-7xl text-green-500 ${font_500}`}>$50 <span className={`text-sm ${font_400}`}>/conference</span></p>
                         </div>
                     </div>
                 </div>
