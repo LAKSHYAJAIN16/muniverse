9ba7d14244027c136de821160e19d398b21562ca
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-10-20 22:07:29 -0400

    finished school information

--- web/pages/on-boarding.js
+++ web/pages/on-boarding.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { font_100, font_200, font_300, font_400, font_500, font_600, font_700, font_800 } from '@/helpers/fonts';
 import Navbar from '@/components/Nav';
+import {
+    CitySelect,
+    CountrySelect,
+    StateSelect,
+} from "react-country-state-city";
+import "react-country-state-city/dist/react-country-state-city.css";
 
 export default function Onboarding() {
+    const [countryid, setCountryid] = useState(0);
+    const [stateid, setstateid] = useState(0);
     return (
         <div>
             <Navbar />
@@ -72,25 +80,52 @@ export default function Onboarding() {
                             </div>
 
                             <div className="mb-4 ">
-                                <label
-                                    className="block text-gray-700 text-md mb-2 "
-                                    htmlFor="name"
-                                >
-                                    school cit
-                                </label>
-                                <input
-                                    className={`px-3 py-2 border border-gray-300 rounded-md outline-none focus:outline-none focus:border-red-500 transition-all text-sm w-96 ${font_300}`}
-                                    type="email"
-                                    id="email"
-                                    name="email"
-                                    placeholder="Enter the official email address of your school"
-                                    required
-                                />
+                                <h6 className='block text-gray-700 text-md mb-2'>school country</h6>
+                                <div className={`w-96 ${font_300} mb-4`}>
+                                    <CountrySelect
+                                        onChange={(e) => {
+                                            setCountryid(e.id);
+                                        }}
+                                        placeHolder="Select Country"
+                                    />
+                                </div>
+                                <h6 className='block text-gray-700 text-md mb-2'>school state</h6>
+                                <div className={`w-96 ${font_300} mb-4`}>
+                                    <StateSelect
+                                        countryid={countryid}
+                                        onChange={(e) => {
+                                            setstateid(e.id);
+                                        }}
+                                        placeHolder="Select State"
+                                    />
+                                </div>
+
+
+                                <h6 className="block text-gray-700 text-md mb-2">school city</h6>
+                                <div className={`w-96 ${font_300}`}>
+                                    <CitySelect
+                                        countryid={countryid}
+                                        stateid={stateid}
+                                        onChange={(e) => {
+                                            console.log(e);
+                                        }}
+                                        placeHolder="Select City"
+                                    />
+                                </div>
+
+                                <h6>hi!</h6>
                             </div>
+                        </div>
+
+                        <div className='mt-10 ml-10'>
+                            <h1 className='mb-5 text-3xl'>Conference Information</h1>
                         </div>
                     </form>
                 </div>
             </div>
+
+            <br />
+            <br />
         </div>
     )
 }
