0cb45efdba88d4f6529e9b1df4f5f0d2747326cc
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-10-30 10:02:38 -0400

    goddamn it reading is so fun ASGSHGSH

--- web/helpers/fauna-client.js
+++ web/helpers/fauna-client.js
@@ -2,7 +2,7 @@ import { Client } from "faunadb"
 import {config} from "dotenv"
 
 function get_client(){
-    config()
+    config();
     const client = new Client({
         secret:"fnAFRH3Mi1AATXdd95NUTyhR6Fml_mAa8zLySBco",
         domain:"db.us.fauna.com"
