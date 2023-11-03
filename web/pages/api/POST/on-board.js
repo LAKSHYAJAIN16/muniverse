f3d46d8875e3276ef596158d6cb30b27683da94b
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-11-03 10:12:19 -0400

    key system? idduno

--- web/pages/api/POST/on-board.js
+++ web/pages/api/POST/on-board.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Create, Collection, Ref } from 'faunadb';
 import { client } from "@/helpers/fauna-client";
+import { scryptSync, randomBytes } from "crypto";
 import { faunaID, uuidThing } from '@/helpers/random-gen';
 
 export default async function handler(req, res) {
     // Assemble Payload
     const id = faunaID();
     const key = uuidThing();
+
+    //Hash key using crypto module
+    const salt = randomBytes(16).toString("hex");
+    const newKey = scryptSync(key, salt, 64).toString(
+        "hex"
+    );
+    const finalKey = `${salt}:${newKey}`;
+
     const payload = {
         ...req.body,
         id: id,
-        key : key,
+        key: finalKey,
     }
     console.log(id);
 
